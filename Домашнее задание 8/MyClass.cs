5124e83ea9d2e0ed082d13947ec970e4e6be8905
Зейтын Бахытбек Кылышбекович <@corp.itstep>
2017-12-13 16:57:19 +0600

    first

---
+++ Домашнее задание 8/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_8
+{
+    class MyClass
+    {
+        private int[] arr = new int[50];
+
+        public int this[int i]
+        {
+            get { return arr[i]; }
+            set { arr[i] = value*value; }
+        }
+    }
+}
---
+++ Домашнее задание 8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass m = new MyClass();
+            m[0] = 5;
+            Console.WriteLine(m[0]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
